59a51dcfebd22eca2b8e63a781e49c14cd2648c2
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-19 11:35:47 +0530

    react Link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 //components
 import Home from './components/home';
 import Posts from './components/posts';
@@ -9,7 +9,18 @@ import Profiles from './components/profiles'
 const App = ()=>{
     return(
         <BrowserRouter>
-            <header> Header   </header>
+            <header>   
+                <Link to='/'>Home</Link>
+                <Link to='/posts'>Posts</Link>
+                <Link to={
+                    {
+                        pathname:'/profiles',
+                        hash : '#hashtag',
+                        search:'?fragment'
+                    }
+                }>Profiles</Link>
+                <hr/>
+            </header>
             <Route path="/" exact component={Home}/>
             <Route path="/posts" component={Posts}/>
             <Route path="/profiles" component={Profiles}/>
